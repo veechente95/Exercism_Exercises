d706b853c074987a26603f75d64a5091bc388122
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-21 22:00:57 -0800

    Create currency_exchange.py

---
+++ currency_exchange.py
@@ -0,0 +1,33 @@
+def exchange_money(budget, exchange_rate):
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+
+    pass
+
+
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
+    pass
+
+
+def get_value_of_bills(denomination, number_of_bills):
+    """
+
+    :param denomination: int - the value of a bill.
+    :param number_of_bills: int - amount of bills you received.
+    :return: int - total value of bills you now have.
+    """
+
+    pass
+
+
