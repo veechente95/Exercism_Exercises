b3ec20269f5f370e978f2a748360bb9091164318
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-11-21 22:01:41 -0800

    Update currency_exchange.py

--- currency_exchange.py
+++ currency_exchange.py
@@ -53,4 +53,14 @@ def get_leftover_of_bills(budget, denomination):
     pass
 
 
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
 
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+
+    pass
