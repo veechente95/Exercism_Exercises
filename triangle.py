5ec01207fdaf314a08ba5a7a7a439338e28ea0e9
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-08 19:45:58 -0800

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,21 @@
+# Determine if a triangle is equilateral, isosceles, or scalene.
+# An equilateral triangle has all three sides the same length.
+# An isosceles triangle has at least two sides the same length. 
+# (It is sometimes specified as having exactly two sides the same length, but for the purposes of this exercise we'll say at least two.)
+# A scalene triangle has all sides of different lengths.
+
+def equilateral(sides):
+    if sides == sides:
+        return True
+    elif sides != sides:
+        return True
+    else:
+        return True
+
+
+def isosceles(sides):
+    pass
+
+
+def scalene(sides):
+    pass
