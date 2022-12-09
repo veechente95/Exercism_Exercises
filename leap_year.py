a45248af639d4847f27f78f67ab77e75d01f0703
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-08 19:03:21 -0800

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -3,25 +3,4 @@
 #     unless the year is also evenly divisible by 400
 
 def leap_year(year):
-    # if year%4==0 and year%100!=0:
-    #     if year%400==0:
-    #         return True
-    # elif year%4!=0:
-    #     return False
-    
-    if year % 4 == 0:
-        return True
-    elif year % 4 != 0:
-        return False
-    elif year % 4 and year % 100 == 0:
-        return False
-    elif year % 4 and year % 100 != 0:
-        return True
-    elif year % 4 and year % 400 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    elif year % 100 == 0 and year % 3 != 0:
-        return False
-    else:
-        return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
