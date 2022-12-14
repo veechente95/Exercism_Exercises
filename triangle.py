2ab00363127a7cd7ad2a23aacbb435944cd60cf2
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-13 21:13:31 -0800

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -3,7 +3,6 @@
 # An isosceles triangle has at least two sides the same length. 
 # (It is sometimes specified as having exactly two sides the same length, but for the purposes of this exercise we'll say at least two.)
 # A scalene triangle has all sides of different lengths.
-
 def equilateral(sides):
     a = sides[0]
     b = sides[1]
@@ -19,10 +18,19 @@ def isosceles(sides):
     a = sides[0]
     b = sides[1]
     c = sides[2]
-    if (a == b or c) or (b == a or c) or (c == a or b):
-        return True]
+    if a == b or c:
+        return True
+    elif sides[2] == a or b or c:
+        return False
 
 
 def scalene(sides):
-    pass
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    if a == b or c:
+        return True
+    elif a != b or c:
+        return False
+
 
