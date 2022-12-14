5d1e274e4252c16c5b1aa47909a7dd7db3c0419c
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-13 21:56:51 -0800

    Update yacht.py

--- yacht.py
+++ yacht.py
@@ -6,23 +6,37 @@
 # If the dice do not satisfy the requirements of the category your solution should return 0. You can assume that five values 
 # will always be presented, and the value of each will be between one and six inclusively. You should not assume that the dice are ordered.
 
+# Score categories.
+# Change the values as you see fit.
+YACHT = 0
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = 7
+FOUR_OF_A_KIND = 8
+LITTLE_STRAIGHT = 9
+BIG_STRAIGHT = 10
+CHOICE = 11
+
+
 def score(dice, category):
-    total = 0
+    for number in dice:
+        counts = [dice.count(number)]
     if category == YACHT:
-        total += 50 if len(set(dice)) == 1 else 0
-    elif category in {ONES, TWOS, THREES, FOURS, FIVES, SIXES}:
-        total += category * sum(throw == category for throw in dice)
-    elif category == FULL_HOUSE:
-        total += sum(dice) if set(Counter(dice).values()) == {2, 3} else 0
-    elif category == FOUR_OF_A_KIND:
-        side, times = Counter(dice).most_common(1)[0]
-        total += 4 * side if times in [4, 5] else 0
-    elif category == LITTLE_STRAIGHT:
-        total += 30 if set(dice) == {1, 2, 3, 4, 5} else 0
-    elif category == BIG_STRAIGHT:
-        total += 30 if set(dice) == {2, 3, 4, 5, 6} else 0
-    elif category == CHOICE:
-        total += sum(dice)
-    else:
-        raise ValueError(f"unknown category: {category}")
-    return total
+        return 50 * (5 in counts)
+    if category < FULL_HOUSE: #ONES - SIXES
+        return category * dice.count(category)
+    if category == FULL_HOUSE:
+        return sum(dice) * (3 in counts and 2 in counts)
+    if category == FOUR_OF_A_KIND:
+        return sorted([number if dice.count(number) > 3 else 0 for number in dice])[-1]*4
+    if category == LITTLE_STRAIGHT:
+        return 30 * (all([count == 1 for count in counts]) and 6 not in dice)
+    if category == BIG_STRAIGHT:
+        return 30 * (all([count == 1 for count in counts]) and 1 not in dice)
+    if category == CHOICE:
+        return sum(dice)
+    return 0
