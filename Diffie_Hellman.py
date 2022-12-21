44bb9faf882d6239acfdd5a66314f7058e416d0d
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-20 23:22:57 -0800

    Create Diffie_Hellman.py

---
+++ Diffie_Hellman.py
@@ -0,0 +1,4 @@
+# Diffie-Hellman key exchange.
+
+# Alice and Bob use Diffie-Hellman key exchange to share secrets. 
+# They start with prime numbers, pick private keys, generate and share public keys, and then generate a shared secret key.
