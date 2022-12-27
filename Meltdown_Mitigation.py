e6cfc1d1031d50aa6a43e91a9a1c3d99f958a4af
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2022-12-26 23:22:14 -0800

    Create Meltdown_Mitigation.py

---
+++ Meltdown_Mitigation.py
@@ -0,0 +1,7 @@
+# Instructions
+# In this exercise, we'll develop a simple control system for a nuclear reactor.
+
+# For a reactor to produce the power it must be in a state of criticality. 
+# If the reactor is in a state less than criticality, it can become damaged. 
+# If the reactor state goes beyond criticality, it can overload and result in a meltdown. 
+# We want to mitigate the chances of meltdown and correctly manage reactor state.
