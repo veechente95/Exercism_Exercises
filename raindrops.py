e9c6e94bf137f3599d91b2ce88e70f4f7e134d9f
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-03 18:33:18 -0800

    Update raindrops.py

--- raindrops.py
+++ raindrops.py
@@ -30,3 +30,14 @@
         return "Plong"
     else:
         return str(number)
+
+    # A more efficient way to write this
+   def convert(number):
+    result = ''
+    if number % 3 == 0:
+        result += 'Pling'
+    if number % 5 == 0:
+        result += 'Plang'
+    if number % 7 == 0:
+        result += 'Plong'
+    return result or str(number)
