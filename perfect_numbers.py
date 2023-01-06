d0c38da683beec7d371afa135bd1eeb2d643f38c
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-01-05 20:31:06 -0800

    Create perfect_numbers.py

---
+++ perfect_numbers.py
@@ -0,0 +1,15 @@
+# Determine if a number is perfect, abundant, or deficient based on Nicomachus' (60 - 120 CE) classification scheme for positive integers.
+
+# Perfect: aliquot sum = number
+  # 6 is a perfect number because (1 + 2 + 3) = 6
+  # 28 is a perfect number because (1 + 2 + 4 + 7 + 14) = 28
+  
+# Abundant: aliquot sum > number
+  # 12 is an abundant number because (1 + 2 + 3 + 4 + 6) = 16
+  # 24 is an abundant number because (1 + 2 + 3 + 4 + 6 + 8 + 12) = 36
+
+# Deficient: aliquot sum < number
+  # 8 is a deficient number because (1 + 2 + 4) = 7
+  # Prime numbers are deficient
+  
+  
