2bfa2349b6223498de2b811b7d38b97d24232fe5
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-02-28 19:20:58 -0800

    Create resistor_color_trio.py

---
+++ resistor_color_trio.py
@@ -0,0 +1,20 @@
+color_dict = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
+def color_code(color):
+    return color_dict[color]
+
+
+def colors():
+    return [key for key in color_dict]
