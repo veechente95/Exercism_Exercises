d0a90c3f4843ec5d5a353f654c1d0af0a7c91cd2
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-03-06 17:18:32 -0800

    Update sublist.py

--- sublist.py
+++ sublist.py
@@ -9,28 +9,25 @@
 # List A is a sublist of B if B contains a sub-sequence of values equal to A.
 
 """This exercise stub and the test suite contain several enumerated constants."""
+SUBLIST = "sublist"
+SUPERLIST = "superlist"
+EQUAL = "equal"
+UNEQUAL = "unequal"
 
-# Possible sublist categories.
-# Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
 
 def sublist(list_one, list_two):
     if list_one == list_two:
-        return None
-
-    if list_one.sort() != list_two.sort():
-        return False
-
-    for num in list_one:
-        if num in list_two == list_one:
-            return True
-
-        elif list_one == list_two:
-            return True
-        else:
-            return None
-        
-   
+        return EQUAL    
+    elif len(list_one) > len(list_two):
+        bigger = list_one
+        smaller = list_two
+    else:
+        bigger = list_two
+        smaller = list_one
+    
+    for i in range(len(bigger)):
+        if bigger[i:i + len(smaller)] == smaller and bigger == list_one:
+            return SUPERLIST
+        elif bigger[i:i + len(smaller)] == smaller and bigger == list_two:
+            return SUBLIST
+    return UNEQUAL
