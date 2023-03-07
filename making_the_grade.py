563079ba0a6bbbff0a719ca0fb9c90f0b5d4de0d
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-03-06 18:35:20 -0800

    Update making_the_grade.py

--- making_the_grade.py
+++ making_the_grade.py
@@ -44,10 +44,14 @@ def student_ranking(student_scores, student_names):
     
 
 def perfect_score(student_info):
-    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
-
-    :param student_info: list - of [<student name>, <score>] lists.
-    :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
-    """
-
-    pass
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam."""
+    perfect = []
+    for name, score in student_info:
+        if score == 100:
+            perfect.append(name)
+            perfect.append(score)
+            break
+        else:
+            continue
+    return perfect
+    
