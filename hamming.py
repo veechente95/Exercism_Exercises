9e1498a36629dff67e4578f1951b7f8976c38066
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-03-09 16:20:22 -0800

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,5 +1,7 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
-        raise ValueError('Length must be equal')
-    errors = [a != b for a, b in zip(strand_a, strand_b)]
-    return sum(errors)
+        raise ValueError("Strands must be of equal length.")
+    # calculates distance by counting 1 for every mis-match
+    return sum(amino_code_a != amino_code_b
+               for amino_code_a, amino_code_b
+               in zip(strand_a, strand_b))
