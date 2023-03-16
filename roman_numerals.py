1521d6771248c9d6c5d2d497edbd95bbb383c77b
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-03-15 22:34:33 -0700

    Update roman_numerals.py

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,1 +1,18 @@
 # Write a function to convert from normal numbers to Roman Numerals.
+
+def roman(number):
+    num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    letters = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+    roman_numeral = ""
+    current_number = number
+    position = 0
+
+    while current_number > 0:
+        if current_number - num[position] >= 0:
+            roman_numeral += letters[position]
+            current_number -= num[position]
+        else:
+            position += 1
+
+    return roman_numeral
